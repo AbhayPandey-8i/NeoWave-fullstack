13a5ce566178c1e9b3613f92f207364ff6075e1f
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-20 21:51:04 +0530

    Initialized Header and Login

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { IoIosArrowDropdown } from "react-icons/io";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='z-10 absolute bg-linear-to-b from-black flex w-full items-center justify-between px-6'>
+      <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" srcset="" />
+      <div className='flex items-center'>
+        <IoIosArrowDropdown color='white' size="24px" />
+        <h1 className='text-lg font-medium text-white'>Abhay-MERN</h1>
+        <div className='ml-4'>
+        <button className='bg-red-800 text-white px-4 py-2'>LogOut</button>
+        <button className='bg-red-800 text-white px-4 py-2 ml-2'>Search Movie</button>
+        </div>
+      </div>
     </div>
   )
 }
--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -4,11 +4,20 @@ import Header from './Header'
 const Login = () => {
     return (
    
-   <div>
+   <div> 
     <Header/>
-    <div>
-        Login
+    <div className='absolute'>
+        <img className='w-screen h-screen bg-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/7ea4545e-42d3-4ebf-82fd-0e1984dc6375/web/IN-en-20260316-TRIFECTA-perspective_789c5633-3949-4708-8e6c-8ddfd22ed696_large.jpg" alt="" srcset="" />
     </div>
+
+     <form className='flex flex-col w-3/12 p-12 my-36 left-0 mx-auto right-0 absolute items-center justify-center bg-black'>
+        <div className='flex flex-col bg-white' >
+            <input type="text" placeholder='Username' />
+            <input type="email" placeholder='Email' />
+            <input type="text" placeholder='Password' />
+        </div>
+     </form>
+
    </div>
 
     )
