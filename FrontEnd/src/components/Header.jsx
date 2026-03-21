7a9a4bfeae40476183a3042f75fe8ad7406daeb8
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-21 16:06:14 +0530

    Done with Login Page

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowDropdown } from "react-icons/io";
 const Header = () => {
   return (
     <div className='z-10 absolute bg-linear-to-b from-black flex w-full items-center justify-between px-6'>
-      <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" srcset="" />
+      <img className='w-56' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
       <div className='flex items-center'>
         <IoIosArrowDropdown color='white' size="24px" />
         <h1 className='text-lg font-medium text-white'>Abhay-MERN</h1>
--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -1,24 +1,69 @@
 import React from 'react'
 import Header from './Header'
+import { useState } from 'react'
 
 const Login = () => {
+
+    const [isLogin, setisLogin] = useState(false)
+    const [FullName, setFullName] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+
+    const loginHandler = () => {
+        setisLogin(!isLogin)
+    }
+
+    const getInputData = (e) => {
+      e.preventDefault()
+      console.log(FullName, email, password)
+
+      setFullName(""),
+      setemail(""),
+      setpassword("")
+    }
+    
+
+
+
+
     return (
-   
-   <div> 
-    <Header/>
-    <div className='absolute'>
-        <img className='w-screen h-screen bg-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/7ea4545e-42d3-4ebf-82fd-0e1984dc6375/web/IN-en-20260316-TRIFECTA-perspective_789c5633-3949-4708-8e6c-8ddfd22ed696_large.jpg" alt="" srcset="" />
-    </div>
 
-     <form className='flex flex-col w-3/12 p-12 my-36 left-0 mx-auto right-0 absolute items-center justify-center bg-black'>
-        <div className='flex flex-col bg-white' >
-            <input type="text" placeholder='Username' />
-            <input type="email" placeholder='Email' />
-            <input type="text" placeholder='Password' />
+        <div>
+            <Header />
+            <div className='absolute'>
+                <img className='w-screen h-screen bg-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/7ea4545e-42d3-4ebf-82fd-0e1984dc6375/web/IN-en-20260316-TRIFECTA-perspective_789c5633-3949-4708-8e6c-8ddfd22ed696_large.jpg" alt="" />
+            </div>
+
+            <form onSubmit={getInputData} className='flex flex-col w-3/12 p-12 my-36 left-0 mx-auto right-0 absolute items-center justify-center bg-black opacity-90 rounded-md'>
+
+                <h1 className='text-white text-3xl mb-5 font-bold' >{isLogin ? "Login" : "Sign up"}</h1>
+
+                <div className='flex flex-col' >
+
+                    {
+                        isLogin && <input value={FullName} onChange={(e) => {
+                            setFullName(e.target.value)
+                        }
+                        } type="text" placeholder='Username' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+                    }
+
+
+                    <input value={email} onChange={(e) => {
+                        setemail(e.target.value)
+                    }
+                    } type="email" placeholder='Email' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+                    <input value={password} onChange={(e) => {
+                        setpassword(e.target.value)
+                    }
+                    } type="password" placeholder='Password' className='outline-none p-3 my-2 rounded-sm bg-gray-800 text-white' />
+
+                  <button className='bg-red-600 mt-6 p-3 text-white rounded-r-sm font-medium '> {isLogin? "Login" : "Signup"} </button>
+
+                    <p className='text-white mt-2' > {isLogin ? "New to Netflix?" : "Already have an account?"} <span onClick={loginHandler} className='ml-1 text-blue-400 font-medium cursor-pointer '> {isLogin ? "Sign up" : "Login"} </span> </p>
+                </div>
+            </form>
+
         </div>
-     </form>
-
-   </div>
 
     )
 }
