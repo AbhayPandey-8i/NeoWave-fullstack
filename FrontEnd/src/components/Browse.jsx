3d3d90f4a8068a4cabc8c24bfc73d76a5c6ad6d7
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-26 00:23:33 +0530

    some more upgrades

--- FrontEnd/src/components/Browse.jsx
+++ FrontEnd/src/components/Browse.jsx
@@ -1,36 +1,44 @@
 import React from 'react'
 import Header from './Header'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import MainContainer from './MainContainer'
 import MovieContainer from './MovieContainer'
+import axios from 'axios'
+import { Now_Playing_Movies, options } from '../../utils/constant'
+import { getNowPlayingMovies } from '../redux/movieSlice'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 
 
 const Browse = () => {
 
-  const user = useSelector(store=>store.app.user)
+  const user = useSelector(store => store.app.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
+  //custom hooks
+  useNowPlayingMovies()
 
   useEffect(() => {
-   if (!user) {
+    if (!user) {
       navigate("/")
+
     }
-
+    // useNowPlayingMovies()
   }, [])
-  
 
-    
+
+
   return (
 
-      
 
-    <div> 
-      <Header/>
+
+    <div>
+      <Header />
       <div>
-        <MainContainer/>
-        <MovieContainer/>
+        <MainContainer />
+        <MovieContainer />
       </div>
     </div>
   )
--- FrontEnd/utils/constant.jsx
+++ FrontEnd/utils/constant.jsx
@@ -1,1 +1,11 @@
 export const API_END_POINT="http://localhost:8080/api/v1/user"
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDc2YTE0ZmJjMGQ4NTMxY2U3ZmQ2OTU3ZWFjMzRmZSIsIm5iZiI6MTc3NDM3ODE2MS4yNDcsInN1YiI6IjY5YzJkY2IxMDRjMDExYmM0MWE0ZTc3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ujVBrhXY5_dhPIkf8k_Fs_ZBTJaVh9nGRUOx7xOp2hs'
+  }
+};
+
+export const Now_Playing_Movies = "https://api.themoviedb.org/3/movie/now_playing"
