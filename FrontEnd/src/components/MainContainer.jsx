8632d6a3615dac734dd3d8d652e0b97bc06119e6
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-30 20:46:27 +0530

    Bug Fixes, Performance Improvement and finishing project

--- FrontEnd/src/components/MainContainer.jsx
+++ FrontEnd/src/components/MainContainer.jsx
@@ -5,27 +5,23 @@ import { useSelector } from 'react-redux'
 
 const MainContainer = () => {
 
-  
-   const movie = useSelector(store=>store.movie?.nowPlayingMovies)
-   console.log("Movies from Redux:", movie)
+  const movie = useSelector(store => store.movie?.nowPlayingMovies)
+  const selectedMovieId = useSelector(store => store.movie.id)
 
-      // const mainMovie = movie?.[0]
-      const mainMovie = movie?.find(m => m.original_language === 'en') || movie?.[0]
+  if (!movie) return
 
-  console.log("Main Movie:", mainMovie)
-  console.log("Movie ID:", mainMovie?.id)
+  const mainMovie = movie?.find(m => m.original_language === 'en') || movie?.[0]
 
-   if (!movie) return // this is called early return in react
+  const selectedMovie = movie?.find(m => m.id === selectedMovieId)
 
-  //  const {overview, id, title} = movie[4]
-  const {overview, id, title} = mainMovie
-    
-   
+  const displayMovie = selectedMovie || mainMovie
+
+  const { overview, id, title } = displayMovie || {}
 
   return (
-    <div >
-        <VideoTitle title={title} overview={overview} />
-      <VideoBackground movieId={mainMovie?.id} />
+    <div>
+      <VideoTitle title={title} overview={overview} />
+      <VideoBackground movieId={displayMovie?.id} />
     </div>
   )
 }
--- FrontEnd/src/components/MovieList.jsx
+++ FrontEnd/src/components/MovieList.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import MovieCard from './MovieCard'
 
 
-const MovieList = ({ title, movies, searchMovie=false }) => {
-    console.log(movies)
-    
+const MovieList = ({ title, movies, searchMovie = false }) => {
+  console.log(movies)
+
   return (
     <div className='px-8'>
-      
+
       <h1 className={`${searchMovie ? "text-black" : " text-white"} text-3xl py-3`}>{title}</h1>
 
       <div className='overflow-x-auto no-scrollbar cursor-pointer flex' >
         <div className='flex items-center'>
-          
+
           {movies?.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} posterpath={movie.poster_path} />
+
+            <MovieCard
+              key={movie.id}
+              movieId={movie.id}   // 🔥 ADD THIS
+              posterpath={movie.poster_path}
+            />
+            
           ))}
 
         </div>
